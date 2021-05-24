976581f9ac00db544b1c5203d12da92f137baf15
doravar <doravar@gmail.com>
2021-05-24 09:52:09 +0200

    fixing integration tests

--- src/test/java/hu/webuni/hr/doravar/web/CompanyControllerIT.java
+++ src/test/java/hu/webuni/hr/doravar/web/CompanyControllerIT.java
@@ -3,87 +3,109 @@ package hu.webuni.hr.doravar.web;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
-import java.util.Collections;
 import java.util.List;
 
-import org.hibernate.mapping.Collection;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
 
 import hu.webuni.hr.doravar.dto.CompanyDto;
 import hu.webuni.hr.doravar.dto.EmployeeDto;
-import hu.webuni.hr.doravar.mapper.CompanyMapper;
-import hu.webuni.hr.doravar.repository.CompanyRepository;
-import hu.webuni.hr.doravar.service.CompanyService;
+import hu.webuni.hr.doravar.dto.HolidayDto;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase
 public class CompanyControllerIT {
 
-	@Autowired
-	CompanyService companyService;
+	private static final String BASE_URI = "/api/companies";
 
 	@Autowired
-	CompanyController companyController;
-
-	@Autowired
-	CompanyRepository companyRepository;
-
-	@Autowired
-	CompanyMapper companyMapper;
+	WebTestClient webTestClient;
 
 	@Test
 	void testAddNewEmployee() throws Exception {
 		CompanyDto company = new CompanyDto(111L, "AlfaTech", "Lisboa");
+		CompanyDto savedCompanyBefore = createCompany(company).expectStatus().isOk().expectBody(CompanyDto.class)
+				.returnResult().getResponseBody();
 		EmployeeDto employee1 = new EmployeeDto("Employee Name1", 100_000, LocalDate.of(2010, 01, 01));
-		EmployeeDto employee2 = new EmployeeDto("Employee Name2", 200_000, LocalDate.of(2013, 02, 01));
-		Long id = companyController.createCompany(company).getId();
-		companyController.addNewEmployee(id, employee1);
-		companyController.addNewEmployee(id, employee2);
-		CompanyDto savedCompany = companyController.getById(id, true);
-		assertThat(savedCompany.getEmployees().size()).isEqualTo(2);
+		Long id = savedCompanyBefore.getId();
+		List<EmployeeDto> listEmployeesBefore = savedCompanyBefore.getEmployees();
+		addNewEmployee(id, employee1);
+		CompanyDto savedCompanyAfter = getCompanyByIdWithEmployees(id);
+		List<EmployeeDto> listEmployeesAfter = savedCompanyAfter.getEmployees();
+		assertThat(listEmployeesBefore.size()).isEqualTo(listEmployeesAfter.size() - 1);
+		assertThat(listEmployeesAfter.contains(employee1));
 	}
 
 	@Test
 	void testReplaceAllEmployee() throws Exception {
 		CompanyDto company = new CompanyDto(111L, "AlfaTech", "Lisboa");
+		long id = createCompany(company).expectStatus().isOk().expectBody(CompanyDto.class).returnResult()
+				.getResponseBody().getId();
 		EmployeeDto employee1 = new EmployeeDto("Employee Name1", 100_000, LocalDate.of(2010, 01, 01));
 		EmployeeDto employee2 = new EmployeeDto("Employee Name2", 200_000, LocalDate.of(2013, 02, 01));
-		Long id = companyController.createCompany(company).getId();
-		companyController.addNewEmployee(id, employee1);
-		companyController.addNewEmployee(id, employee2);
+		addNewEmployee(id, employee1);
+		addNewEmployee(id, employee2);
+
 		EmployeeDto employee3 = new EmployeeDto("Employee Name3", 300_000, LocalDate.of(2010, 03, 01));
 		EmployeeDto employee4 = new EmployeeDto("Employee Name4", 400_000, LocalDate.of(2013, 04, 01));
 		List<EmployeeDto> newListOfEmployees = List.of(employee3, employee4);
-		companyController.replaceEmployees(id, newListOfEmployees);
-		List<EmployeeDto> savedEmployees = companyController.getById(id, true).getEmployees();
+		replaceEmployees(id, newListOfEmployees);
 
-		assertThat(savedEmployees).isEqualTo(newListOfEmployees);
+		List<EmployeeDto> savedEmployees = getCompanyByIdWithEmployees(id).getEmployees();
 
+		assertThat(savedEmployees).usingElementComparatorIgnoringFields("id", "companyName")
+				.isEqualTo(newListOfEmployees);
 	}
 
 	@Test
 	void testDeleteEmployee() throws Exception {
 		CompanyDto company = new CompanyDto(111L, "AlfaTech", "Lisboa");
+		long companyId = createCompany(company).expectStatus().isOk().expectBody(CompanyDto.class).returnResult()
+				.getResponseBody().getId();
 		EmployeeDto employee1 = new EmployeeDto("Employee Name1", 100_000, LocalDate.of(2010, 01, 01));
 		EmployeeDto employee2 = new EmployeeDto("Employee Name2", 200_000, LocalDate.of(2013, 02, 01));
 		EmployeeDto employee3 = new EmployeeDto("Employee Name3", 300_000, LocalDate.of(2010, 03, 01));
-		Long idOfCompany = companyController.createCompany(company).getId();
-		companyController.addNewEmployee(idOfCompany, employee1);
-		companyController.addNewEmployee(idOfCompany, employee2);
-		companyController.addNewEmployee(idOfCompany, employee3);
-
-		List<EmployeeDto> listOfEmployeesBeforeDeleting = companyController.getById(idOfCompany, true).getEmployees();
-
-		long idOfEmployee = listOfEmployeesBeforeDeleting.get(0).getId();
-
-		companyController.deleteEmployee(idOfCompany, idOfEmployee);
-
-		List<EmployeeDto> listOfEmployeesAfterDeleting = companyController.getById(idOfCompany, true).getEmployees();
-
-		assertThat(listOfEmployeesAfterDeleting.contains(listOfEmployeesBeforeDeleting.get(0))).isFalse();
-		assertThat(listOfEmployeesAfterDeleting.size()).isEqualTo(listOfEmployeesBeforeDeleting.size() - 1);
+		addNewEmployee(companyId, employee1);
+		addNewEmployee(companyId, employee2);
+		addNewEmployee(companyId, employee3);
+		List<EmployeeDto> savedEmployeesBefore = getCompanyByIdWithEmployees(companyId).getEmployees();
+		EmployeeDto deletedEmployee = savedEmployeesBefore.get(0);
+		long employeeId = deletedEmployee.getId();
+		deleteEmployee(companyId, employeeId);
+		List<EmployeeDto> savedEmployeesAfter = getCompanyByIdWithEmployees(companyId).getEmployees();
+		assertThat(savedEmployeesAfter.contains(deletedEmployee)).isFalse();
+		assertThat(savedEmployeesAfter.size()).isEqualTo(savedEmployeesBefore.size() - 1);
 	}
+
+	private ResponseSpec createCompany(CompanyDto company) {
+		return webTestClient.post().uri(BASE_URI).bodyValue(company).exchange();
+	}
+
+	private ResponseSpec addNewEmployee(long id, EmployeeDto employeeDto) {
+		String path = BASE_URI + "/" + id + "/addNewEmployee";
+		return webTestClient.post().uri(path).bodyValue(employeeDto).exchange();
+	}
+
+	private CompanyDto getCompanyByIdWithEmployees(long id) {
+		String path = BASE_URI + "/" + id;
+		return webTestClient.get().uri(uriBuilder -> uriBuilder.path(path).queryParam("full", true).build()).exchange()
+				.expectStatus().isOk().expectBody(CompanyDto.class).returnResult().getResponseBody();
+	}
+
+	private ResponseSpec replaceEmployees(long id, List<EmployeeDto> employees) {
+		String path = BASE_URI + "/" + id + "/replaceEmployees";
+		return webTestClient.put().uri(path).bodyValue(employees).exchange();
+	}
+
+	private CompanyDto deleteEmployee(long companyId, long employeeId) {
+		String path = BASE_URI + "/" + companyId + "/employees/" + employeeId;
+		return webTestClient.delete().uri(path).exchange().expectStatus().isOk().expectBody(CompanyDto.class)
+				.returnResult().getResponseBody();
+	}
+
 }
--- src/test/java/hu/webuni/hr/doravar/web/HolidayControllerIT.java
+++ src/test/java/hu/webuni/hr/doravar/web/HolidayControllerIT.java
@@ -9,6 +9,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hu.webuni.hr.doravar.dto.CompanyDto;
 import hu.webuni.hr.doravar.dto.EmployeeDto;
@@ -17,33 +24,71 @@ import hu.webuni.hr.doravar.model.StateOfHolidayRequest;
 import hu.webuni.hr.doravar.repository.HolidayRepository;
 import hu.webuni.hr.doravar.service.HolidayService;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase
 public class HolidayControllerIT {
 
+	private static final String BASE_URI = "/api/holidays";
+
 	@Autowired
-	HolidayController holidayController;
+	WebTestClient webTestClient;
 
 	@Test
 	void testThatCreatedHolidayIsListed() throws Exception {
-		HolidayDto holiday = new HolidayDto(4L, 5L, LocalDate.of(2021, 01, 01), LocalDate.of(2021, 01, 20));
-		List<HolidayDto> listBefore = holidayController.getHolidays();
-		Long id = holidayController.createHoliday(holiday).getId();
-		HolidayDto savedHoliday = holidayController.getById(id);
-		List<HolidayDto> listAfter = holidayController.getHolidays();
-		assertThat(savedHoliday).usingRecursiveComparison().ignoringFields("id").isEqualTo(holiday);
-		assertThat(listBefore.size() + 1).isEqualTo(listAfter.size());
+		HolidayDto holiday = new HolidayDto(4L, 5L, LocalDate.of(2022, 01, 01), LocalDate.of(2022, 01, 20));
+		HolidayDto savedHoliday = createHoliday(holiday).expectStatus().isOk().expectBody(HolidayDto.class)
+				.returnResult().getResponseBody();
+		List<HolidayDto> holidays = getHolidays();
+		assertThat(holidays.contains(savedHoliday));
 	}
 
 	@Test
 	void testApproveHoliday() throws Exception {
-		HolidayDto holiday = new HolidayDto(4L, 5L, LocalDate.of(2021, 01, 01), LocalDate.of(2021, 01, 20));
-		Long id = holidayController.createHoliday(holiday).getId();
-		HolidayDto savedHoliday = holidayController.getById(id);
-		assertThat(savedHoliday.getStateOfHolidayRequest()).isEqualTo(StateOfHolidayRequest.PENDING);
-		holidayController.approveHoliday(id, "APPROVED");
-		savedHoliday = holidayController.getById(id);
+		HolidayDto holiday = new HolidayDto(4L, 5L, LocalDate.of(2022, 01, 01), LocalDate.of(2022, 01, 20));
+		long holidayId = createHoliday(holiday).expectStatus().isOk().expectBody(HolidayDto.class).returnResult()
+				.getResponseBody().getId();
+		approveHoliday(holidayId, "APPROVED");
+		HolidayDto savedHoliday = getById(holidayId);
 		assertThat(savedHoliday.getStateOfHolidayRequest()).isEqualTo(StateOfHolidayRequest.APPROVED);
+	}
+
+	@Test
+	void tesModifytHoliday() throws Exception {
+		HolidayDto holiday = new HolidayDto(4L, 5L, LocalDate.of(2022, 01, 01), LocalDate.of(2022, 01, 20));
+		HolidayDto savedHolidaybefore = createHoliday(holiday).expectStatus().isOk().expectBody(HolidayDto.class)
+				.returnResult().getResponseBody();
+		savedHolidaybefore.setEndOfHoliday(LocalDate.of(2022, 10, 20));
+		modifyHoliday(savedHolidaybefore);
+		HolidayDto savedHolidayAfter = getById(savedHolidaybefore.getId());
+		assertThat(savedHolidayAfter.getEndOfHoliday()).isEqualTo(LocalDate.of(2022, 10, 20));
+	}
+
+	private ResponseSpec createHoliday(HolidayDto holiday) {
+		return webTestClient.post().uri(BASE_URI).bodyValue(holiday).exchange();
+	}
+
+	private List<HolidayDto> getHolidays() {
+		return webTestClient.get().uri(BASE_URI).exchange().expectStatus().isOk().expectBodyList(HolidayDto.class)
+				.returnResult().getResponseBody();
+	}
+
+	private ResponseSpec approveHoliday(long id, String stateOfHolidayRequest) {
+		String path = BASE_URI + "/approval/" + id;
+		return webTestClient.put().uri(
+				uriBuilder -> uriBuilder.path(path).queryParam("stateOfHolidayRequest", stateOfHolidayRequest).build())
+				.exchange();
+	}
+
+	private HolidayDto getById(long id) {
+		String path = BASE_URI + "/" + id;
+		return webTestClient.get().uri(path).exchange().expectStatus().isOk().expectBody(HolidayDto.class)
+				.returnResult().getResponseBody();
+	}
+
+	private HolidayDto modifyHoliday(HolidayDto holiday) {
+		String path = BASE_URI + "/" + holiday.getId();
+		return webTestClient.put().uri(path).bodyValue(holiday).exchange().expectStatus().isOk()
+				.expectBody(HolidayDto.class).returnResult().getResponseBody();
 	}
 
 }
